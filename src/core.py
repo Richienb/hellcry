c48028e528f8e941c778c146696ef0b28ba7ab95
Richie Bendall <richiebendall@gmail.com>
2018-12-05 17:58:21 +1300

    Update files

--- src/core.py
+++ src/core.py
@@ -1,9 +1,15 @@
 """
 The main core file
 """
-from fabulous import image
 import quilt_lang as _
-import importlib
-print(image.Image(_.absolutepath("../Graphics/Hellcry_logo (1).png")))
+
+try:
+    from fabulous import image
+    print(image.Image(_.absolutepath("../assets/banner-background.png")))
+except ModuleNotFoundError:
+    print("HellCry")
+
 print("Loading...")
+temp_dir = _.tempdir()
+
 import game
